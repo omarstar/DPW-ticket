f60885cb6ca148732882af1ed3c703ccd668baea
Omar Barakat <obarakat12@gmail.com>
2024-02-13 17:44:20 +0400

    define locals en & ar

--- Front-End/src/utils/language.js
+++ Front-End/src/utils/language.js
@@ -1,17 +1,189 @@
+//<Text name="" />
 const locals = {
     'getStarted' : {
         "en" : "Get started",
         "ar" : "ابدأ هنا"
     },
-    'getStarted2' : {
-        "en" : "Get started2",
-        "ar" : "ابد2أ هنا"
+    'titleSelectOptions' : {
+        "en" : "Please select an option",
+        "ar" : "الرجاء تحديد الإختيار"
     },
-    'getStarted' : {
-        "en" : "Get started",
-        "ar" : "ابدأ هنا"
+    'titleEnterMobile' : {
+        "en" : "Please enter your mobile number",
+        "ar" : "الرجاء إدخال رقم هاتفك المتحرك"
     },
-
+    'titleEnterOtp' : {
+        "en" : "Please enter the OTP sent to your mobile number",
+        "ar" : "الرجاء إدخال رمز المرور المرسل الى رقم هاتفك المتحرك"
+    },
+    'noteReceiveOtp' : {
+        "en" : "Didn't receive OTP?",
+        "ar" : "لم تستلم رمز المرور لمرة واحدة؟"
+    },
+    'titleExitModal' : {
+        "en" : "Are you sure you want to cancel and start the process over again?",
+        "ar" : "هل تريد إلغاء العملية والبدء من جديد؟"
+    },
+    'titleSelectCategory' : {
+        "en" : "Please select a category",
+        "ar" : "الرجاء تحديد الفئة"
+    },
+    'titleSelectService' : {
+        "en" : "Please select a service",
+        "ar" : "الرجاء تحديد الخدمة"
+    },
+    'titleServiceModal' : {
+        "en" : "Service info",
+        "ar" : "معلومات عن الخدمة"
+    },
+    'titlePosition' : {
+        "en" : "Your position in line",
+        "ar" : "مكانك في قائمة الإنتظار"
+    },
+    'titleTurn' : {
+        "en" : "It's your turn",
+        "ar" : "حان دورك الحين"
+    },
+    'noteProceedto' : {
+        "en" : "Please proceed to",
+        "ar" : "الرجاء التوجه الى"
+    },
+    'titleThankyou' : {
+        "en" : "Thank you for visiting us today.",
+        "ar" : "شكرا لكم على الزيارة."
+    },
+    'titleSelectApp' : {
+        "en" : "Please select your confirmed face-to-face appointment to check-in.",
+        "ar" : "الرجاء إختيار الموعد المؤكد للتسجيل"
+    },
+    'noteAppCheckin' : {
+        "en" : "You can check-in from 20 minutes before the time of the appointment.",
+        "ar" : "بإستطاعتك التسجيل قبل الموعد ب 20 دقيقة."
+    },
+    'modalAppEarly' : {
+        "en" : "It is not time to check-in to your appointment yet",
+        "ar" : "لم يحن الوقت لتسجيل الوصول الى موعدك بعد"
+    },
+    'modalAppLate' : {
+        "en" : "You have missed your appointment",
+        "ar" : "لقد فاتك الموعد"
+    },
+    'noteShowResults' : {
+        "en" : "Showing results for ",
+        "ar" : "عرض النتائج لفرع "
+    },
+    'titleBookingSummary' : {
+        "en" : "Your appointment summary",
+        "ar" : "تسجيل وصول للموعد"
+    },
+    'btnWalkinFlow' : {
+        "en" : "Walk-in",
+        "ar" : "خدمة حضور"
+    },
+    'btnContinue' : {
+        "en" : "Continue",
+        "ar" : "استمرار"
+    },
+    'btnAppFlow' : {
+        "en" : "Check-in appointment",
+        "ar" : "تسجيل وصول للموعد"
+    },
+    'btnSearchContinue' : {
+        "en" : "Search & Continue",
+        "ar" : "البحث والإستمرار"
+    },
+    'btnConfirmProceed' : {
+        "en" : "Confirm & proceed",
+        "ar" : "التأكيد والمتابعة"
+    },
+    'btnResendOtp' : {
+        "en" : "Resend OTP",
+        "ar" : "إعادة أرسال رمز المرور"
+    },
+    'btnSubmit' : {
+        "en" : "Submit",
+        "ar" : "إرسال"
+    },
+    'btnClose' : {
+        "en" : "Close",
+        "ar" : "إغلاق"
+    },
+    'btnProceedWalkin' : {
+        "en" : "Proceed as walk-in",
+        "ar" : "المتابعة لخدمة الحضور"
+    },
+    'btnReturnHomepage' : {
+        "en" : "Return to homepage",
+        "ar" : "العودة إلى الصفحة الرئيسية"
+    },
+    'btnYes' : {
+        "en" : "Yes",
+        "ar" : "نعم"
+    },
+    'btnNo' : {
+        "en" : "No",
+        "ar" : "لا"
+    },
+    'txtYou' : {
+        "en" : "you",
+        "ar" : "أنت"
+    },
+    'txtName' : {
+        "en" : "Name",
+        "ar" : "الاسم"
+    },
+    'txtMobile' : {
+        "en" : "mobile",
+        "ar" : "الهاتف الجوال"
+    },
+    'txtEmail' : {
+        "en" : "E-mail",
+        "ar" : "البريد الإلكتروني"
+    },
+    'txtCompany' : {
+        "en" : "Company",
+        "ar" : "الشركة"
+    },
+    'txtBranch' : {
+        "en" : "Branch name",
+        "ar" : "الفرع"
+    },
+    'txtService' : {
+        "en" : "Service name",
+        "ar" : "الخدمة"
+    },
+    'txtAppDatetime' : {
+        "en" : "Appointment date / time",
+        "ar" : "تاريخ ووقت الموعد"
+    },
+    'alertEmptyField' : {
+        "en" : "This field is required",
+        "ar" : "هذه الخانة مطلوبة"
+    },
+    'alertWrongMobile' : {
+        "en" : "Wrong mobile number",
+        "ar" : "رقم هاتفك المتحرك غير صحيح"
+    },
+    'alertInvalidFormat' : {
+        "en" : "Invalid format",
+        "ar" : "صيغة غير صالحة"
+    },
+    'alertNetwork' : {
+        "en" : "Network temporarily unavailable",
+        "ar" : "الشبكة غير متاحة مؤقتًا"
+    },
+    'alertNoAppWrongMobile' : {
+        "en" : "Wrong mobile number or no appointment found",
+        "ar" : "لا يوجد مواعيد او رقم هاتفك المتحرك غير صحيح"
+    },
+    'alertWrongOtp' : {
+        "en" : "Wrong OTP number",
+        "ar" : "رمز مرور غير صحيح"
+    },
+    'alertNoRecords' : {
+        "en" : "No records found",
+        "ar" : "لم يتم العثور على السجلات"
+    }
 }
 
 
@@ -19,3 +191,10 @@ export function getLocalTranslate(key,lang) {
     // console.log(locals[key]);
     return lang == 'en' ? locals[key]["en"] : locals[key]["ar"];
 }
+
+/*
+},
+'selectOptions' : {
+    "en" : "wrd",
+    "ar" : ""
+    */
