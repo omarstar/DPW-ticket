8511646ae4ea3dfc5404a7fcb3b0ed82d36549b0
Omar Barakat <obarakat12@gmail.com>
2024-02-14 23:08:58 +0400

    list of services for testing

--- Front-End/src/utils/constants.js
+++ Front-End/src/utils/constants.js
@@ -144,3 +144,214 @@ export const appiontmentsList = [
         }
     }
 ]
+
+const servicesList = [
+    {
+        "id": 54,
+        "internalName": "Lease Related Services",
+        "externalName": "Lease Related Services",
+        "internalDescription": "To follow up on an active Jafza Service Request (SR) or address any general enquiries related to Contracts & Leasing.\n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    },
+    {
+        "id": 46,
+        "internalName": "Customer Service — Downsize / Exit Interviews",
+        "externalName": "Customer Service — Downsize / Exit Interviews",
+        "internalDescription": "A meeting with Jafza Customer Service to clarify queries related to company downsizing and or attend exit interviews.\n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    },
+    {
+        "id": 48,
+        "internalName": "Customer Service — Payments / Company Reinstatement",
+        "externalName": "Customer Service — Payments / Company Reinstatement",
+        "internalDescription": "A meeting with Jafza Customer Service to clarify queries related to payments and company reinstatement.\n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    },
+    {
+        "id": 60,
+        "internalName": "Other Customer Services (NOCS, Complaints, etc.)",
+        "externalName": "Other Customer Services (NOCS, Complaints, etc.)",
+        "internalDescription": "A face-to-face meeting with Jafza Customer Service to clarify queries related to NOCs, complaints and others. \n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    },
+    {
+        "id": 38,
+        "internalName": "Admin Services - Employee affairs",
+        "externalName": "Admin Services - Employee affairs",
+        "internalDescription": "To follow up on an active Jafza Service Request (SR) or address any general enquiries related to employee affairs.\n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    },
+    {
+        "id": 50,
+        "internalName": "Government Affairs & Golden Visa",
+        "externalName": "Government Affairs & Golden Visa",
+        "internalDescription": "An online meeting to follow up on an existing Jafza Service Request (SR) or address any enquiries related to Government Affairs & Golden visa.\n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    },
+    {
+        "id": 58,
+        "internalName": "Manpower Contract Attestation",
+        "externalName": "Manpower Contract Attestation",
+        "internalDescription": "A planned face-to-face meeting with a Jafza expert for the purpose of completing contract attestation.\n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    },
+    {
+        "id": 40,
+        "internalName": "APM Service - EOL Doc Submission",
+        "externalName": "APM Service - EOL Doc Submission",
+        "internalDescription": "A face-to-face meeting with the Jafza Asset & Property Management Team to submit your company's End-of-Lease documents.\n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    },
+    {
+        "id": 42,
+        "internalName": "APM Service - Key Handover",
+        "externalName": "APM Service - Key Handover",
+        "internalDescription": "A face-to-face meeting with the Jafza Asset & Property Management Team to hand over your facility keys.\n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    },
+    {
+        "id": 44,
+        "internalName": "APM Service - NOC Clarifications",
+        "externalName": "APM Service - NOC Clarifications",
+        "internalDescription": "A meeting with the Jafza Asset & Property Management Team to address queries regarding NOCs.\n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    },
+    {
+        "id": 68,
+        "internalName": "APM Service - NOC Submission/Collection",
+        "externalName": "APM Service - NOC Submission/Collection",
+        "internalDescription": "A face-to-face meeting with the Jafza Asset & Property Management Team to deliver or collect your company NOC documents. \n",
+        "externalDescription": null,
+        "targetTransactionTime": 600
+    },
+    {
+        "id": 69,
+        "internalName": "Jafza - License Enquiry",
+        "externalName": "Jafza - License Enquiry",
+        "internalDescription": "An online meeting to follow up on an existing Jafza Service Request (SR) or address any enquiries related to Jafza license.\n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    },
+    {
+        "id": 70,
+        "internalName": "NIP - License Enquiry",
+        "externalName": "NIP - License Enquiry",
+        "internalDescription": "An online meeting to follow up on an existing Jafza Service Request (SR) or address any enquiries related to NIP license.\n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    },
+    {
+        "id": 62,
+        "internalName": "Registration & Offshore Services",
+        "externalName": "Registration & Offshore Services",
+        "internalDescription": "A meeting with a Jafza expert to clarify queries related to Registration and offshore services.\n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    },
+    {
+        "id": 64,
+        "internalName": "Courier Collection/Delivery - Offshore",
+        "externalName": "Courier Collection/Delivery - Offshore",
+        "internalDescription": "A planned face-to-face meeting with a Jafza expert for exchanging Offshore company documents.\n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    },
+    {
+        "id": 71,
+        "internalName": "Registration/Offshore - Signature Verification",
+        "externalName": "Registration/Offshore - Signature Verification",
+        "internalDescription": "A planned face-to-face meeting with a Jafza expert for the purpose of completing Signature verification.\n",
+        "externalDescription": null,
+        "targetTransactionTime": 600
+    },
+    {
+        "id": 72,
+        "internalName": "New Company Set-up",
+        "externalName": "New Company Set-up",
+        "internalDescription": "A meeting with the Jafza Sales Team to discuss the requirements for setting up a new company.\n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    },
+    {
+        "id": 73,
+        "internalName": "Existing Company Expansion",
+        "externalName": "Existing Company Expansion",
+        "internalDescription": "A meeting with the Jafza Sales Team to discuss your company's expansion needs.\n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    },
+    {
+        "id": 74,
+        "internalName": "Follow-up - Service Request/Lead",
+        "externalName": "Follow-up - Service Request/Lead",
+        "internalDescription": "A meeting with the Jafza Sales Team to follow-up on your new company set-up or company expansion requests.\n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    },
+    {
+        "id": 75,
+        "internalName": "Document Collection / Submission",
+        "externalName": "Document Collection / Submission",
+        "internalDescription": "A face-to-face meeting with the Jafza Sales Team to deliver or collect your company set-up /expansion documents. \n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    },
+    {
+        "id": 77,
+        "internalName": "WPS Service",
+        "externalName": "WPS Service",
+        "internalDescription": "An online meeting to follow up on an existing Jafza Service Request (SR) or address any enquiries related to WPS services.\n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    },
+    {
+        "id": 78,
+        "internalName": "Labour Dispute",
+        "externalName": "Labour Dispute",
+        "internalDescription": "An online meeting to follow up on an existing Jafza Service Request (SR) or address any enquiries related to Labour disputes.\n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    },
+    {
+        "id": 79,
+        "internalName": "Workforce Affairs - General Enquiry",
+        "externalName": "Workforce Affairs - General Enquiry",
+        "internalDescription": "An online meeting to follow up on an existing Jafza Service Request (SR) or address any enquiries related to Workforce affairs.\n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    },
+    {
+        "id": 80,
+        "internalName": "Workforce Affairs - Company Enquiry",
+        "externalName": "Workforce Affairs - Company Enquiry",
+        "internalDescription": "An online meeting to follow up on an existing Jafza Service Request (SR) or address any workforce affair enquiries related to your company. \n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    },
+    {
+        "id": 81,
+        "internalName": "Admin Doc - Collection/Submission",
+        "externalName": "Admin Doc - Collection/Submission",
+        "internalDescription": "A face-to-face meeting with the Jafza Employee Affairs Team to deliver or collect your company or employee documents. \n",
+        "externalDescription": null,
+        "targetTransactionTime": 900
+    },
+    {
+        "id": 76,
+        "internalName": "General Sales Enquiry",
+        "externalName": "General Sales Enquiry",
+        "internalDescription": "A meeting with the Jafza Sales Team to address any general queries you have about Jafza and its product solutions. \n",
+        "externalDescription": null,
+        "targetTransactionTime": 0
+    }
+]
